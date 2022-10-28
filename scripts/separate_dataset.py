c5a87adb3705960e1d04eff311343a80bbca56a4
cpondoc <chris.pondoc@comcast.net>
2022-10-27 20:46:24 -0700

    create test and train full datasets

--- scripts/separate_dataset.py
+++ scripts/separate_dataset.py
@@ -17,7 +17,22 @@ def create_dalle_split():
                     shutil.copy('data/dalle/' + folder + '/' + img, 'data/test/dalle/' + img)
                 counter += 1
 
+def create_non_dalle_split():
+    img_folders = os.listdir('data/non-dalle')
+    counter = 0
+    for folder in img_folders:
+        if (folder[0] != "."):
+            imgs = os.listdir('data/non-dalle/' + folder)
+            for img in imgs:
+                if (counter < 7897):
+                    shutil.copy('data/non-dalle/' + folder + '/' + img, 'data/train/non-dalle/' + img)
+                else:
+                    shutil.copy('data/non-dalle/' + folder + '/' + img, 'data/test/non-dalle/' + img)
+                counter += 1
+
+
 def main():
     create_dalle_split()
+    create_non_dalle_split()
 
 main()
