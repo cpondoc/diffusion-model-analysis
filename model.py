5cbc9ec81f3df6a3ee034ad9b9b8f58484f3dffd
cpondoc <chris.pondoc@comcast.net>
2022-10-27 20:26:44 -0700

    create the individual datasets

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 '''
-Initial example from Pytorch documentation
+Initial training of the data
 '''
 
 # First, import the necessary libraries
+from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -11,34 +12,43 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import os
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+from skimage import io, transform
+from torch.utils.data import Dataset, DataLoader
+from torchvision import utils
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+class DalleDataset(Dataset):
+    """DALLE dataset."""
 
-def imshow(img):
-    img = img / 2 + 0.5 # un-normalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    def __init__(self, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.transform = transform
+        self.all_data = []
+        dalle_folders = (os.listdir('data/dalle'))
+        for folder in dalle_folders:
+            if (folder[0] != "."):
+                images = (os.listdir('data/dalle/' + folder))
+                for image in images:
+                    img_name = 'data/dalle/' + folder + '/' + image
+                    torch_img = io.imread(img_name)
+                    sample = {'image': torch_img, 'label': 1}
+                    self.all_data.append(sample)
 
-def visualize_dataset(features):
-    imshow(torchvision.utils.make_grid(features))
+    def __len__(self):
+        return len(self.all_data)
+
+    def __getitem__(self, id):
+        if (self.transform):
+            img = self.transform(self.all_data[id]["image"])
+            return (img, self.all_data[id]["label"])
+        return (self.all_data[id]["image"], self.all_data[id]["label"])
 
 def main():
     # Data transformation -- turn into a PyTorch Tensor, normalize the data (mean, std)
@@ -46,84 +56,15 @@ def main():
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # Loading in train data
-    # Note: no num_workers due to issues with multi-processing
-    batch_size = 4
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+    # Loading in initial training data
+    dalle_data = DalleDataset(transform=transform)
+    batch_size = 20
+    trainloader = torch.utils.data.DataLoader(dalle_data, batch_size=batch_size,
                                             shuffle=True)
 
-    # Loading in test data
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=False)
-
-    classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-    # Displaying a sample image (squeeze helps to )
+    # Iterating through everything
     dataiter = iter(trainloader)
     features, labels = next(dataiter)
+    print(labels)
 
-    # Creating the CNN
-    net = Net()
-
-    # Loss Function and Optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(2):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
-
-
-    print('Finished Training')
-
-    # Saving trained model
-    PATH = './cifar_net.pth'
-    torch.save(net.state_dict(), PATH)
-
-    # Test Loading
-    dataiter = iter(testloader)
-    images, labels = next(dataiter)
-
-    # Loading in a new example of the neural net, and loading in the weights
-    net = Net()
-    net.load_state_dict(torch.load(PATH))
-
-    # Getting accuracy of the data
-    correct = 0
-    total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            # calculate outputs by running images through the network
-            outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 main()
-    
